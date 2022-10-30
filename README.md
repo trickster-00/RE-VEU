d413159068ebb1fdc49b9b50ee3b24d6c3253865
Manaal Saxena <saxenamanaal@gmail.com>
2022-10-30 12:20:59 +0530

    initial load
    publishing repository

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# RE-VEU
- A efficient solution to help you understand your customer feedback better.
+
+<h1 align="center">
+  <br>
+  <a href="http://www.github.com/trickster-00"><img src="src/logo-white.png" alt="RE-VEU" width="200"></a>
+  <br>
+  RE-VEU
+  <br>
+</h1>
+
+<h4 align="center">A powerful web application built on top of <a href="https://streamlit.io" target="_blank">Streamlit</a>.</h4>
+
+<p align="center">
+  <a href="https://badge.fury.io/js/electron-markdownify">
+    <img src="https://badge.fury.io/js/electron-markdownify.svg"
+         alt="Gitter">
+  </a>
+  <a href="https://gitter.im/amitmerchant1990/electron-markdownify"><img src="https://badges.gitter.im/amitmerchant1990/electron-markdownify.svg"></a>
+  <a href="https://saythanks.io/to/bullredeyes@gmail.com">
+      <img src="https://img.shields.io/badge/SayThanks.io-%E2%98%BC-1EAEDB.svg">
+  </a>
+  <a href="https://www.paypal.me/AmitMerchant">
+    <img src="https://img.shields.io/badge/$-donate-ff69b4.svg?maxAge=2592000&amp;style=flat">
+  </a>
+</p>
+
+<p align="center">
+  <a href="#key-features">Key Features</a> •
+  <a href="#how-to-use">How To Use</a> •
+  <a href="#credits">Credits</a> •
+  <a href="#related">Related</a> •
+  <a href="#license">License</a>
+</p>
+
+![screenshot](https://raw.githubusercontent.com/amitmerchant1990/electron-markdownify/master/app/img/markdownify.gif)
+
+## Key Features
+
+* LivePreview - Make changes, See changes
+  - Instantly see what your Markdown documents look like in HTML as you create them.
+* Sync Scrolling
+  - While you type, LivePreview will automatically scroll to the current location you're editing.
+* GitHub Flavored Markdown  
+* Syntax highlighting
+* [KaTeX](https://khan.github.io/KaTeX/) Support
+* Dark/Light mode
+* Toolbar for basic Markdown formatting
+* Supports multiple cursors
+* Save the Markdown preview as PDF
+* Emoji support in preview :tada:
+* App will keep alive in tray for quick usage
+* Full screen mode
+  - Write distraction free.
+* Cross platform
+  - Windows, macOS and Linux ready.
+
+## How To Use
+
+To run this application, you'll need [python](https://www.python.org/downloads/) installed on your computer. From your command line:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Credits
+
+This software uses the following open source packages:
+
+- [Electron](http://electron.atom.io/)
+- [Node.js](https://nodejs.org/)
+- [Marked - a markdown parser](https://github.com/chjj/marked)
+- [showdown](http://showdownjs.github.io/showdown/)
+- [CodeMirror](http://codemirror.net/)
+- Emojis are taken from [here](https://github.com/arvida/emoji-cheat-sheet.com)
+- [highlight.js](https://highlightjs.org/)
+
+
+## related
+
+- [Domainify](https://github.com/trickster-00/Domainify) - A cybersecurity app
+- [CPC](https://github.com/trickster-00/Customer-Personality-Clustering) - An algorithm to gain understand about your customers
+
+## License
+
+MIT
+
+---
+
+> [manaal.online](https://trickster-00.github.io/Manaal/index.html) &nbsp;&middot;&nbsp;
+> GitHub [@trickster-00](https://github.com/trickster-00) &nbsp;&middot;&nbsp;
+> LinkedIn [@manaalsaxena](https://linkedin.com/in/manaalsaxena)
---
+++ app.py
@@ -0,0 +1,157 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import textblob
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
+import re
+
+nltk.download('stopwords')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+analyzer = SentimentIntensityAnalyzer()
+
+st.title("RE-VEU")
+st.header("Customer feedback is very Important. \n We help you get better insights about your reviews.")
+st.markdown("Let's get started, please upload a file with reviews")
+
+
+uploaded_file = st.file_uploader(label="Upload a File",
+                                 type=['csv','xlsx'])
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write('## Dataset')
+    st.dataframe(data)
+
+    st.write('Select the column you want to analyse')
+    col = st.selectbox("Column Name",(data.columns))
+
+    if st.button("Click for Results") :
+        
+
+        def CleanText(df):
+            tempText = []
+            for line in df:
+                text = line
+                text = re.sub(r"[^a-zA-Z0-9]+", ' ' ,text.lower())
+                tempText.append(text)
+
+            return tempText
+        
+        text = data[col].apply(str)
+        data['Text'] = CleanText(text)
+        data['Reviews'] = CleanText(text)
+
+        def short_text(df):
+            tempText = []
+            for line in df:
+                text = line
+                stopword = set(stopwords.words('english'))
+                words =  word_tokenize(text)
+                text = [w for w in words if w not in stopword]
+                text = ' '.join(text)
+                tempText.append(text)
+            return tempText
+
+        data['Reviews'] = short_text(data['Reviews'])
+
+        def sentiment(df):
+            tempList = []
+            for line in df:
+                text = line
+                check = analyzer.polarity_scores(text)
+                if check['compound'] >= 0.05:
+                    tempList.append("Positive")
+                elif check['compound'] <= - 0.05:
+                    tempList.append("Negative")
+                else:
+                    tempList.append("Neutral")
+
+            return tempList
+        
+        data['Sentiment'] = sentiment(data['Reviews'])
+
+        def service(df):
+            service = []
+            for line in df:
+                words = word_tokenize(line)
+                text = nltk.pos_tag(words)
+                tempdict = dict(text)
+                vals = list(tempdict.values())
+                prouns = ['VB','VBD','VBG','VBN']
+                templis = []
+                for i in vals:
+                    for j in prouns:
+                            if i == j:
+                                templis.append('Yes')
+                            else:
+                                templis.append('No')
+                service.append(len(list(filter(lambda x: x == 'Yes', templis))))
+            return service
+
+        def product(df):
+            product = []
+            for line in df:
+                words = word_tokenize(line)
+                text = nltk.pos_tag(words)
+                tempdict = dict(text)
+                vals = list(tempdict.values())
+                strs = ' '.join([str(elem) for elem in vals])
+                prouns = ['NN','NNS']
+                templis = []
+                for i in vals:
+                    for j in prouns:
+                        if i == j:
+                            templis.append('Yes')
+                        else:
+                            templis.append('No')
+
+                product.append(len(list(filter(lambda x: x == 'Yes', templis))))
+
+            return product
+
+        def staff(df):
+            staff = []
+            for line in df:
+                words = word_tokenize(line)
+                text = nltk.pos_tag(words)
+                tempdict = dict(text)
+                vals = list(tempdict.values())
+                strs = ' '.join([str(elem) for elem in vals])
+                prouns = ['NNP','NNPS','PRP','PRP$']
+                templis = []
+                for i in vals:
+                    for j in prouns:
+                        if i == j:
+                            templis.append('Yes')
+                        else:
+                            templis.append('No')
+                staff.append(len(list(filter(lambda x: x == 'Yes', templis))))
+
+            return staff
+
+        data['Service'] = service(data["Text"])
+        data['Product'] = product(data["Text"])
+        data['Staff'] = staff(data["Text"])
+
+        final_df = data[['Reviews','Sentiment','Service','Product','Staff']]
+
+        positive = len(data[data['Sentiment'] == 'Positive'])
+        neutral = len(data[data['Sentiment'] == 'Neutral'])
+        negative = len(data[data['Sentiment'] == 'Negative'])
+
+        st.write(f" Positive Reviews : {positive} | Neutral Reviews : {neutral} | Negative Reviews : {negative}")
+        st.dataframe(final_df)
+
+        st.download_button(
+            label="Download data as CSV",
+            data= final_df.to_csv().encode("utf-8"),
+            file_name='data.csv',
+            mime='text/csv',
+        )
